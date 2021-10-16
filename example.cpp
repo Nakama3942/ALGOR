03a3a851119ab34f4e1e79afd5783078a94197a2
Valentin <nakama3942@gmail.com>
2021-10-16 21:59:58 +0300

    Обновил пример
    Убрал кучу старого повторяющегося кода и добавил примеры для нового функционала

--- example.cpp
+++ example.cpp
@@ -1,146 +1,58 @@
 #include <iostream>
 using namespace std;
 
-#include "sortingalgorithms.h"
+#include "sortingalgorithms.hpp"
 
 const int g_arraySize = 1024;
-
-template <typename T>
-void printer(const char *printing_string, T *arr)
-{
-    cout << printing_string;
-    SortingAlgorithms::ArrayProcessing<int>::print(arr, g_arraySize);
-    cout << "\n";
-}
 
 int main()
 {
     int *array = new int[g_arraySize];
 
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nСортировка вставками\n";
-
     //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 10, 999);
+    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 1000);
+
+    //Копирую массив
+    int *array1 = new int[g_arraySize];
+    SortingAlgorithms::ArrayProcessing<int>::copy(array1, array, g_arraySize);
+
     //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
+    SortingAlgorithms::ArrayProcessing<int>::print("Сгенерированный массив: ", array1, g_arraySize);
+
     //Сортирую массив
-    SortingAlgorithms::InsertSort<int>::insert_sort(array, g_arraySize);
+    SortingAlgorithms::InsertSort<int>::insert_sort(array1, g_arraySize);
+    //SortingAlgorithms::BubbleSort<int>::bubble_sort(array1, g_arraySize);
+    //SortingAlgorithms::CocktailShakerSort<int>::cocktail_shaker_sort(array1, g_arraySize);
+    //SortingAlgorithms::MergeSort<int>::merge_sort(array1, 0, g_arraySize - 1);
+    //SortingAlgorithms::HeapSort<int>::heap_sort(array1, g_arraySize);
+    //SortingAlgorithms::QuickSort<int>::quick_sort(array1, 0, g_arraySize - 1);
+    //SortingAlgorithms::CountingSort::counting_sort(array1, g_arraySize);
+    //SortingAlgorithms::RadixSort::radix_sort(array1, g_arraySize);
+
     //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
+    SortingAlgorithms::ArrayProcessing<int>::print("Отсортированный массив: ", array1, g_arraySize);
 
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
+    //Нахожу сумму
+    int sum;
+    SortingAlgorithms::ArrayProcessing<int>::sum(array1, g_arraySize, sum);
+    cout << "Сумма элементов: " << sum << "\n";
 
-    cout << "\nПузырьковая сортировка\n";
+    //Нахожу среднее арифметическое
+    int average;
+    SortingAlgorithms::ArrayProcessing<int>::average(array1, g_arraySize, average);
+    cout << "Среднее арифметическое: " << average << "\n";
 
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::BubbleSort<int>::bubble_sort(array, g_arraySize);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
+    //Нахожу медиану
+    int mediana;
+    SortingAlgorithms::ArrayProcessing<int>::mediana(array1, g_arraySize, mediana);
+    cout << "Медиана: " << mediana << "\n";
 
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
+    //Нахожу моду с количеством
+    int moda, moda_count;
+    SortingAlgorithms::ArrayProcessing<int>::moda(array1, g_arraySize, moda, moda_count);
+    cout << "Чаще всего встречается элемент: " << moda << ", а именно " << moda_count << " раз.\n";
 
-    cout << "\nСортировка Шейкером\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::CocktailShakerSort<int>::cocktail_shaker_sort(array, g_arraySize);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nСортировка слиянием\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::MergeSort<int>::merge_sort(array, 0, g_arraySize - 1);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nСортировка Пирамидой (двоичной кучей)\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::HeapSort<int>::heap_sort(array, g_arraySize);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nБыстрая сортировка\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::QuickSort<int>::quick_sort(array, 0, g_arraySize - 1);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nCортировка подсчётом\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::CountingSort<int>::counting_sort(array, g_arraySize);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
-    cout << "\nПоразраядная (радиальная) сортировка\n";
-
-    //Генерирую (заполняю) массив
-    SortingAlgorithms::ArrayProcessing<int>::generatedArray(array, g_arraySize, 1, 20);
-    //Вывожу на экран массив
-    printer<int>("Сгенерированный массив: ", array);
-    //Сортирую массив
-    SortingAlgorithms::RadixSort<int>::radix_sort(array, g_arraySize);
-    //Вывожу на экран массив
-    printer<int>("Отсортированный массив: ", array);
-
-    //=====================================================================================
-    //=====     +++++     =====     +++++     =====     +++++     =====     +++++     =====
-    //=====================================================================================
-
+    delete[] array1;
     delete[] array;
 
     return 0;
