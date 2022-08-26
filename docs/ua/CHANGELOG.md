2cc4e6f49fc63da34773f21c2d99ae10e34013a3
Kalynovsky Valentin <nakama3942@gmail.com>
2022-08-26 16:11:50 +0300

    Завершив ліцензіонування
    Завершив ліцензіонування

--- docs/ua/CHANGELOG.md
+++ docs/ua/CHANGELOG.md
@@ -1,10 +1,24 @@
 # CHANGELOG
+<!--
+Copyright [yyyy] [name of copyright owner]
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
 ## v1.0.0.0 (29.08.2022)
 
 > *Задокументовано усе на світі*...
 
-### Documenting:
+### Документування:
 - Повністю у всіх деталях задокументовано увесь функціонал бібліотеки до найменших деталей!
 - Написано багато прикладів, що описують увесь функціонал бібліотеки у всіх деталях.
 - Переписано README.md: дописано багато важливих деталей типу Ліценції, Збірки бібліотеки тощо.
@@ -13,7 +27,7 @@
 - Створено сайт з документацією та додано посилання до репозиторію.
 - Документацію перекладено на англійську мову.
 
-### Enhancements:
+### Покращення:
 - Було перерозподілено файлову структуру проекту:
 	- Файли заголовків розташовуються у піддиректорії *include*;
 	- Файли реалізацій розташовуються у піддиректорії *src*;
--- example/Algor – template.cpp
+++ example/Algor – template.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
-using std::cin;
-using std::cout;
+/* ******************************   ALGOR.cpp   ******************************* *
+ * ---------------------------------------------------------------------------- *
+ *                                                                              *
+ * Copyright © 2021-2022 Kalynovsky Valentin. All rights reserved.              *
+ *                                                                              *
+ * Licensed under the Apache License, Version 2.0 (the "License");              *
+ * you may not use this file except in compliance with the License.             *
+ * You may obtain a copy of the License at                                      *
+ *                                                                              *
+ *     http://www.apache.org/licenses/LICENSE-2.0                               *
+ *                                                                              *
+ * Unless required by applicable law or agreed to in writing, software          *
+ * distributed under the License is distributed on an "AS IS" BASIS,            *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     *
+ * See the License for the specific language governing permissions and          *
+ * limitations under the License.                                               *
+ *                                                                              *
+ * ---------------------------------------------------------------------------- *
+ * **************************************************************************** */
 
-#include "../src/ALGOR.hpp"
+#include <iostream>
+
+#include "../include/ALGOR.hpp"
+using namespace ALGOR;
 
 int main()
 {
@@ -12,6 +31,6 @@ int main()
 
 	//! [words]
 
-	cin.get();
+	std::cin.get();
 	return 0;
 }
